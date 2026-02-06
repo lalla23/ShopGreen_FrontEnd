dbd5c170ab011677039c986a05a90f23c5c83d9c
Anna Luvisotto <anna.luvisotto@studenti.unitn.it>
2026-02-06 18:39:03 +0100

    eliminazione api key

--- constants.ts
+++ constants.ts
@@ -1,5 +1,10 @@
 
-import { Shop, ShopCategory, ShopStatus, Zone, Seller, Notification, NotificationType } from './types';
+import { Zone } from './types';
 
-// Center of Trento
 export const TRENTO_CENTER = { lat: 46.06787, lng: 11.12108 };
+
+export const MOCK_ZONES: Zone[] = [
+  { id: 'z1', name: 'Centro Storico', center: { lat: 46.067, lng: 11.121 } },
+  { id: 'z2', name: 'Povo', center: { lat: 46.066, lng: 11.150 } },
+  { id: 'z3', name: 'Trento Nord', center: { lat: 46.085, lng: 11.115 } },
+];
